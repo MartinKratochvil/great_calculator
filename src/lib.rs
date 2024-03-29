b2e1c1edbae5ac44f78757330b7123310fb091ff
honzahodak <honzahodak@seznam.cz>
2024-03-29 23:50:17 +0100

    Added mockup of all math functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 
-///takes two float values and returns their sum
-///
+/// Function requires two arguments of type f64
+/// returns arg1 + arg2
 /// ```
 /// // Some documentation.
 /// let x = 23.0;
@@ -14,15 +14,13 @@ pub fn add(left: f64, right: f64) -> f64{
     left + right
 }
 
-/// returns left - right
-///
+/// Function requires two arguments of type f64
+/// returns arg1 - arg2
 /// ```
 /// let x = 36;
 /// let y = 16;
 /// let sum = great_calculator::sub(x as f64,y as f64);
 /// assert_eq!(sum,20f64);
-/// ```
-/// ```
 /// assert_eq!(great_calculator::sub(20f64,13.0), 7.0);
 /// ```
 pub fn sub(left: f64, right: f64) -> f64{
@@ -32,9 +30,11 @@ pub fn sub(left: f64, right: f64) -> f64{
 /// Function requires two arguments of type f64
 /// returns arg1 * arg2
 /// ```
-/// let y = 15.0;
-/// let x = 10.0;
+/// let x = 15.0;
+/// let y = 10.0;
 /// let result = great_calculator::mul(x,y);
+/// assert_eq!(result,150.0);
+/// let result = great_calculator::mul(y,x);
 /// assert_eq!(result,150.0);
 /// ```
 pub fn mul(left: f64, right:f64) -> f64{
@@ -43,8 +43,8 @@ pub fn mul(left: f64, right:f64) -> f64{
 /// Function requires two arguments of type f64
 /// returns arg1 / arg2
 /// ```
-/// let y = 40.0;
-/// let x = 10.0;
+/// let x = 40.0;
+/// let y = 10.0;
 /// let result = great_calculator::div(x,y);
 /// assert_eq!(result,4.0);
 /// let result = great_calculator::div(y,x);
@@ -55,6 +55,35 @@ pub fn mul(left: f64, right:f64) -> f64{
 pub fn div(left: f64, right:f64) -> f64{
     left/right
 }
+
+pub fn pwr(left: f64, right:f64) -> f64{
+    left + right
+}
+
+pub fn sqrt(left: f64, right:f64) -> f64{
+    left + right
+}
+
+pub fn fact(left: f64, right:f64) -> f64{
+    left + right
+}
+
+pub fn sin(left: f64, right:f64) -> f64{
+    left + right
+}
+pub fn cos(left: f64, right:f64) -> f64{
+    left + right
+}
+pub fn tan(left: f64, right:f64) -> f64{
+    left + right
+}
+pub fn cotg(left: f64, right:f64) -> f64{
+    left + right
+}
+
+
+
+
 
 
 
