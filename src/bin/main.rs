8b56132ceaeab184b239bbd74e76c251c0c10335
Nitram293 <nitram293@gmail.com>
2024-03-29 16:03:51 +0100

    move main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,3 @@
-use great_calculator::*;
-
-
 fn main() {
-    hello();
-    great_calculator::add(15f64,17f64);
-}
-
-///
-/// ```
-/// println!("this is test");
-/// assert_eq(hello(), 15);
-/// ```
-fn hello() -> u32{
     println!("Hello, world!");
-    15
 }
