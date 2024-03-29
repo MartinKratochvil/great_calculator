0fc8b31f387f7929da3ddf73beca32eb15e6159b
nexu <moatttyas@seznam.cz>
2024-03-29 16:02:23 +0100

    Založení knihovny, složky tests, moovenutí main.rs do složky src/bin

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,17 @@
+use great_calculator::*;
+
 
 fn main() {
+    hello();
+    great_calculator::add(15f64,17f64);
+}
+
+///
+/// ```
+/// println!("this is test");
+/// assert_eq(hello(), 15);
+/// ```
+fn hello() -> u32{
+    println!("Hello, world!");
+    15
 }
