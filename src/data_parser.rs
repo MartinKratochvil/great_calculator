cb587408304d16d41fc77e6e8bc3d19ca577f0c7
nexu <moatttyas@seznam.cz>
2024-03-30 20:07:39 +0100

    Rozdělaná InfixToPrefix funkce

--- src/data_parser.rs
+++ src/data_parser.rs
@@ -6,8 +6,53 @@ enum InfixToken{
     Slash,
     Sin,
     Cos,
+    Caret,
+    Root,
+    ExclamMark,
     RightParethesies,
     LeftParenthesies,
+}
+
+impl InfixToken{
+    fn ToPrefix(&self) -> PrefixToken{
+        match self {
+            InfixToken::Value(i) => PrefixToken::Value(*i),
+            InfixToken::Plus => PrefixToken::Plus,
+            InfixToken::Minus => PrefixToken::Minus,
+            InfixToken::Star => PrefixToken::Star,
+            InfixToken::Slash => PrefixToken::Slash,
+            InfixToken::Sin => PrefixToken::Sin,
+            InfixToken::Cos => PrefixToken::Cos,
+            InfixToken::Caret => PrefixToken::Caret,
+            InfixToken::Root => PrefixToken::Root,
+            InfixToken::ExclamMark => PrefixToken::ExclamMark,
+            _ => panic!(),
+        }
+    }
+    fn Unary(&self) -> bool{
+        match self {
+            InfixToken::Sin => true,
+            InfixToken::Cos => true,
+            _ => false,
+        }
+    }
+    fn Binary(&self) -> bool{
+        match self{
+            InfixToken::Plus => true,
+            InfixToken::Minus => true,
+            InfixToken::Star => true,
+            InfixToken::Slash => true,
+            InfixToken::Caret => true,
+            InfixToken::Root => true,
+            _ => false,
+        }
+    }
+    fn Value(&self) -> bool{
+        match self {
+            InfixToken::Value(i) => true,
+            _ => false,
+        }
+    }
 }
 
 enum PrefixToken{
@@ -18,6 +63,53 @@ enum PrefixToken{
     Slash,
     Sin,
     Cos,
+    Caret,
+    Root,
+    ExclamMark,
+}
+
+impl PrefixToken{
+    fn make_prefix(mut infix: Vec<InfixToken>) -> Vec<PrefixToken>{
+        let mut stack: Vec<InfixToken> = Vec::new();
+        let mut reversePrefix: Vec<PrefixToken> = Vec::new();
+        infix.reverse();
+        while !infix.is_empty() {
+            match infix.last(){
+                Some(infixToken) => {
+                    match infixToken {
+                        InfixToken::Value(i) => reversePrefix.push(infixToken.ToPrefix()),
+                        InfixToken::Plus => {
+                            match stack.last() {
+                                Some(prefixToken) => {
+                                    match prefixToken {
+                                        PrefixToken::Caret => reversePrefix.push(stack.pop()),
+                                        PrefixToken::Root => reversePrefix.push(stack.pop()),
+                                        PrefixToken::Slash => reversePrefix.push(stack.pop()), // tady se to musí převést ze Some<T> na T
+                                        PrefixToken::Star => reversePrefix.push(stack.pop()),
+                                        _ => reversePrefix.push(infixToken.ToPrefix()),
+                                    }
+                                }
+                                None => stack.push(infixToken.ToPrefix()),
+                            }
+                        },
+                        InfixToken::Minus => {},
+                        InfixToken::Star => {},
+                        InfixToken::Slash => {},
+                        InfixToken::Sin => {},
+                        InfixToken::Cos => {},
+                        InfixToken::Caret => {},
+                        InfixToken::Root => {},
+                        InfixToken::ExclamMark => {},
+                        InfixToken::RightParethesies => {},
+                        InfixToken::LeftParenthesies => {},
+                    }
+                }
+                None => panic!(),
+            };
+            infix.pop();
+        }
+        reversePrefix
+    }
 }
 
 enum BinaryFunctions {
@@ -25,6 +117,8 @@ enum BinaryFunctions {
     Sub,
     Mul,
     Div,
+    Pow,
+    Root,
 }
 
 enum UnaryFunctions{
@@ -65,4 +159,6 @@ enum Expression {
     Value(f64),
 }
 
+fn TestF(){
 
+}
