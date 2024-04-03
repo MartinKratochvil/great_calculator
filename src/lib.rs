c6b7d168b9725c111fae5ad3fb085ee46c9f4304
honzahodak <honzahodak@seznam.cz>
2024-04-03 15:46:30 +0200

    Added test for all math functions (Most functions are not yet implemented)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-
-
 /// Function requires two arguments of type f64
 /// returns arg1 + arg2
 /// ```
@@ -40,6 +38,7 @@ pub fn sub(left: f64, right: f64) -> f64{
 pub fn mul(left: f64, right:f64) -> f64{
     left*right
 }
+
 /// Function requires two arguments of type f64
 /// returns arg1 / arg2
 /// ```
@@ -48,7 +47,7 @@ pub fn mul(left: f64, right:f64) -> f64{
 /// let result = great_calculator::div(x,y);
 /// assert_eq!(result,4.0);
 /// let result = great_calculator::div(y,x);
-/// assert_eq!(result,0.25);///
+/// assert_eq!(result,0.25);
 /// let result = great_calculator::div(x,x);
 /// assert_eq!(result,1.0);
 /// ```
@@ -56,33 +55,114 @@ pub fn div(left: f64, right:f64) -> f64{
     left/right
 }
 
+/// Function requires two arguments of type f64
+/// returns arg1^{arg2}
+/// ```
+/// let x = 2.0;
+/// let y = 5.0;
+/// let result = great_calculator::pwr(x,y);
+/// assert_eq!(result,32.0);
+/// let result = great_calculator::pwr(y,x);
+/// assert_eq!(result,25.0);
+/// let x = 10.0;
+/// let y = 5.0;
+/// let result = great_calculator::pwr(y,x);
+/// assert_eq!(result,9765625.0);
+/// let result = great_calculator::pwr(x,y);
+///  assert_eq!(result,100000.0);
+/// ```
 pub fn pwr(left: f64, right:f64) -> f64{
     left + right
 }
 
+/// Function requires two arguments of type f64
+/// returns arg2-th Sqrt of arg1
+/// ```
+/// let n = 2.0;
+/// let x = 16.0;
+/// let result = great_calculator::sqrt(x,n);
+/// assert_eq!(result,4.0);
+/// let x = 1024.0;
+/// let result = great_calculator::pwr(x,n);
+/// assert_eq!(result,32.0);
+/// let x = 4096.0;
+/// let n = 4.0;
+/// let result = great_calculator::pwr(x,n);
+/// assert_eq!(result,8.0);
+/// let x = 27.0;
+/// let n = 3.0;
+/// let result = great_calculator::pwr(x,n);
+///  assert_eq!(result,3.0);
+/// ```
 pub fn sqrt(left: f64, right:f64) -> f64{
     left + right
 }
 
-pub fn fact(left: f64, right:f64) -> f64{
-    left + right
+/// Function requires one argument of type f64
+/// returns factorial of arg1...arg1!
+/// ```
+/// let x = 3.0;
+/// let result = great_calculator::fact(x);
+/// assert_eq!(result,6.0);
+/// let x = 5.0;
+/// let result = great_calculator::fact(x);
+/// assert_eq!(result,120.0);
+/// let x = 10.0;
+/// let result = great_calculator::fact(x);
+/// assert_eq!(result,3628800.0);
+/// let x = 1.0;
+/// let result = great_calculator::fact(x);
+///  assert_eq!(result,1.0);
+/// ```
+pub fn fact(x: f64) -> f64{
+    x
 }
 
-pub fn sin(left: f64, right:f64) -> f64{
-    left + right
-}
-pub fn cos(left: f64, right:f64) -> f64{
-    left + right
-}
-pub fn tan(left: f64, right:f64) -> f64{
-    left + right
-}
-pub fn cotg(left: f64, right:f64) -> f64{
-    left + right
+/// Function requires one argument of type f64
+/// returns sine of arg1...sin(arg1)
+/// ```
+/// use great_calculator::sin;
+/// let x = 0.0;
+/// let result = great_calculator::sin(x);
+/// assert_eq!(result,0.0);
+/// let x = 90.0;
+/// let result = great_calculator::sin(x);
+/// assert_eq!(result,1.0);
+/// let x = 45.0;
+/// let result = great_calculator::sin(x);
+/// let refresult = f64::sin(x.to_radians());
+/// assert_eq!(result,refresult);
+/// let x = 60.0;
+/// let refresult = f64::sin(x.to_radians());
+/// let result = great_calculator::sin(x);
+///  assert_eq!(result,refresult);
+/// ```
+pub fn sin(x: f64) -> f64{
+    x
 }
 
-
-
+/// Function requires one argument of type f64
+/// returns cosine of arg1...cos(arg1)
+/// ```
+/// use great_calculator::sin;
+/// let x = 0.0;
+/// let result = great_calculator::cos(x);
+/// assert_eq!(result,1.0);
+/// let x = 90.0;
+/// let result = great_calculator::cos(x);
+/// assert_eq!(result,0.0);
+/// let x = 45.0;
+/// let result = great_calculator::cos(x);
+/// let refresult = f64::cos(x.to_radians());
+/// assert_eq!(result,refresult);
+/// let x = 60.0;
+/// let refresult = f64::cos(x.to_radians());
+/// let result = great_calculator::cos(x);
+///  assert_eq!(result,refresult);
+/// ```
+pub fn cos(x: f64) -> f64{
+    x
+}
 
 
 
