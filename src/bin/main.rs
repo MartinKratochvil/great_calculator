8283d8b744062c6f7cc4a44419ae2b9640e4d7f7
Nitram293 <nitram293@gmail.com>
2024-04-23 23:11:37 +0200

    fix inputting wrong expressions

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -73,37 +73,50 @@ fn main() {
 #[allow(unused_variables)]
 fn handle_number(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
     if let Some(digit) = label.chars().last() {
+        match tokens.last() {
+            Some(Token::RightParentheses) => return,
+            Some(Token::Exclamation) => return,
+            _ => {}
+        }
         num_buff.push(digit);
         println!("add: {}", label);
         //TODO: print digit
     }
+
 }
 
 #[allow(unused_variables)]
 fn handle_point(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
-    if let Some(dec_point) = label.chars().last() {
-        if !num_buff.contains(dec_point) {
-            num_buff.push(dec_point);
-            println!("add: {}", label);
-            //TODO: print dot
+    if !num_buff.chars().last().is_none() {
+        if let Some(dec_point) = label.chars().last() {
+            if !num_buff.contains(dec_point) {
+                num_buff.push(dec_point);
+                println!("add: {}", label);
+                //TODO: print dot
+            }
+
         }
     }
 }
 
 fn handle_binary_func(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
-    if push_num_buff(tokens, num_buff) {
-        println!("push: {}", label.as_str());
-        tokens.push(get_token_from_str(label));
-        //TODO: print operation
+    if !push_num_buff(tokens, num_buff) && tokens.last().is_none() {
+        return;
     }
+
+    println!("push: {}", label.as_str());
+    tokens.push(get_token_from_str(label));
+    //TODO: print operation
 }
 
 fn handle_minus(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
-    if push_num_buff(tokens, num_buff) {
-        println!("push: {}", label.as_str());
-        tokens.push(get_token_from_str(label));
-        //TODO: print operation
+    if !push_num_buff(tokens, num_buff) && tokens.last().is_none() {
+        return;
     }
+
+    println!("push: {}", label.as_str());
+    tokens.push(get_token_from_str(label));
+    //TODO: print operation
 }
 
 fn handle_gon_func(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
@@ -121,17 +134,25 @@ fn handle_factorial(tokens: &mut Vec<Token>, num_buff: &mut String, label: Strin
 }
 
 fn handle_open_bracket(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
-    push_num_buff(tokens, num_buff);
-    println!("push: {}", label);
-    tokens.push(get_token_from_str(label));
-    //TODO: print bracket
+    if !push_num_buff(tokens, num_buff) {
+        match tokens.last() {
+            Some(Token::RightParentheses) => return,
+            Some(Token::Exclamation) => return,
+            _ => {}
+        }
+        println!("push: {}", label);
+        tokens.push(get_token_from_str(label));
+        //TODO: print bracket
+
+    }
 }
 
 fn handle_close_bracket(tokens: &mut Vec<Token>, num_buff: &mut String, label: String) {
-    push_num_buff(tokens, num_buff);
-    println!("push: {}", label);
-    tokens.push(get_token_from_str(label));
-    //TODO: print bracket
+    if push_num_buff(tokens, num_buff) {
+        println!("push: {}", label);
+        tokens.push(get_token_from_str(label));
+        //TODO: print bracket
+    }
 }
 
 #[allow(unused_variables)]
